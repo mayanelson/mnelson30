0e55e3b8b34886dd091b460c07a6ca90ccc57d33
Maya Nelson <mnelson30@stuy.edu>
2022-09-29 09:11:48 -0400

    functio9n should work, no txt file

--- 05_bitstream/krewes.py
+++ 05_bitstream/krewes.py
@@ -1,4 +1,4 @@
-import random
+from random import randint
 
 krewes = {2:["NICHOLAS",  "ANTHONY",  "BRIAN",  "SAMUEL",  "JULIA",  "YUSHA",  "CORINA",  "CRAIG",  "FANG MIN",  "JEFF",  "KONSTANTIN",  "AARON",  "VIVIAN",  "AYMAN",  "TALIA",  "FAIZA",  "ZIYING",  "YUK KWAN",  "DANIEL",  "WEICHEN",  "MAYA",  "ELIZABETH",  "ANDREW",  "VANSH",  "JONATHAN",  "ABID",  "WILLIAM",  "HUI",  "ANSON",  "KEVIN",  "DANIEL",  "IVAN",  "JASMINE",  "JEFFREY"],
  7:["DIANA",  "DAVID",  "SAM",  "PRATTAY",  "ANNA",  "JING YI",  "ADEN",  "EMERSON",  "RUSSELL",  "JACOB",  "WILLIAM",  "NADA",  "SAMANTHA",  "IAN",  "MARC",  "ANJINI",  "JEREMY",  "LAUREN",  "KEVIN",  "RAVINDRA",  "SADI",  "EMILY",  "GITAE",  "MAY",  "MAHIR",  "VIVIAN",  "GABRIEL",  "BRIANNA",  "JUN HONG",  "JOSEPH",  "MATTHEW",  "JAMES",  "THOMAS",  "NICOLE",  "Karen"], 
@@ -8,10 +8,10 @@ krewes = {2:["NICHOLAS",  "ANTHONY",  "BRIAN",  "SAMUEL",  "JULIA",  "YUSHA",  "
 with open('krewes.txt') as file:
     contents = file.read()
     devos = contents.split("@@@")
-    for i in range(devos):
+    for i in range(len(devos)):
         devo = devos[i].split("$$$")
         devolist = krewes[int(devo[0])]
-        for j in range(devolist):
+        for j in range(len(devolist)):
             if devolist[j] == devo[1]:
                 devolist[j] += " " + devo[2]
         krewes[int(devo[0])] = devolist
@@ -26,4 +26,10 @@ def selectDevo():
     devoList = krewes[randomCrew]
 
     randomDevo = randint(0,len(devoList)-1)
-    return devoList[randomDevo]
+    devothing = devoList[randomDevo].split(" ")
+    print("Name:" + devothing[0] + " Period: " + str(randomCrew) + " Ducky:" + devothing[1])
+ 
+
+print(krewes)
+for i in range(10):
+    selectDevo()
