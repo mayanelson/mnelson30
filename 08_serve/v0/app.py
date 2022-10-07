4839b07a997449d1ef08fa7aa64711d9301e6c34
mayanelson <90730676+mayanelson@users.noreply.github.com>
2022-10-07 09:13:58 -0400

    Update app.py

--- 08_serve/v0/app.py
+++ 08_serve/v0/app.py
@@ -2,30 +2,57 @@
 ZIMZIM(mermann Telegram) Ziying Jian, Maya Nelson, Ivan Yeung
 SoftDev
 K08 -- Flask
-2022-10-04
-time spent:
+2022-10-06
+time spent: 1 hr
 '''
 
-from flask import Flask
-app = Flask(__name__) #Creates a Flask application called __name__
-
-@app.route("/") #This is a route
-def hello_world():
-    print(__name__) #Prints "__main__" in the terminal
-    return "No hablo queso!"  #Prints "No hablo queso" in th website
-
-app.run()  #run() runs the function
-
 '''
-NOTABLES:
-* When we replaced __name__ with 'foo', we didn't receive an error and the "No hablo queso" was still printed!
-In the terminal, we received this: Serving Flask app 'foo'
-PREDICTIONS:
-* The name of the Flask application is printed in the terminal.
+DISCOS:
+- In order to have information displayed on a webpage, use HTML markers wrapped in Strings. 
+For instance, we figured out that in order to create a line break, you must used "<br>", not "\n"
+- In a module, there must only be one return statement culminating all the information to be displayed on the webpage.
 QCC:
-* How is hello_world() being run using run()? Does run() run all the functions in the file?
-* What do the underscores denote in __name__?
-* Running app.py on my personal machine doesn't create a hyperlink (but still opens if pasted in browser!)
-* What does the above DISCO mean?!
-* As a programmer, how can we judge when to name the app and when to insert this special Python variable?
+- What does debug do?
+- Why doesn't the page display the result of multiple functions? 
+- Is printing the __name__ necessary?
 '''
+
+import random
+
+from csv import reader
+
+jobs = []
+weight = []
+
+with open('occupations.csv','r') as file:
+    contents = reader(file)
+    count = 0
+    rcount = 0
+    for row in contents: #get the amount of rows to ignore the last line
+        rcount +=1
+    file.seek(0) # resets reader back to beginning
+    for row in contents:
+        if count != 0 and count != rcount-1:
+            jobs.append(row[0])
+            weight.append(int(float(row[1])*10)) #typecasts as float rather than string, then coverts to int
+        count+=1
+    
+        
+#print(random.choices(jobs, weights = weight)) #random.choices takes weight into account for each value
+    
+from flask import Flask
+app = Flask(__name__) #create instance of class Flask
+
+@app.route("/")       #assign fxn to route
+
+def hello_world():
+    print(__name__)
+    teamName = "ZIMZIM(mermann Telegram): Ziying Jian, Maya Nelson, Ivan Yeung" + "<br>"
+    selectedOccup = "<br>" +"Selected occupation: " + random.choices(jobs, weights = weight)[0] + "<br>"
+     #Prints the only element in the list
+    listJobs = "<br>" + "List of jobs: " + str(jobs) 
+    return teamName + selectedOccup + listJobs
+
+if __name__ == "__main__":  # true if this file NOT imported
+    app.debug = True        # enable auto-reload upon code change
+    app.run()
