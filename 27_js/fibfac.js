71b4a10f3fc0805f277ca72369e90bffa1d97d42
Maya Nelson <mnelson30@stuy.edu>
2023-04-04 09:26:25 -0400

    fibby fact

--- 27_js/fibfac.js
+++ 27_js/fibfac.js
@@ -9,5 +9,26 @@
 // pair programming style,
 // implement a fact and fib fxn
 
+function factorial(num) {
+    if (num < 0) 
+          return -1;
+    else if (num == 0) 
+        return 1;
+      else {
+          return (num * factorialize(num - 1));
+      }
+
+function fib(num){
+  var a = 1, b = 0, tem;
+
+  while (num >= 0){
+    tem = a;
+    a = a + b;
+    b = tem;
+    num--;
+  }
+
+  return b;
+}
 
 //Do whatever you think is needed. Think: S I M P L E.   Think: S M A R T.
