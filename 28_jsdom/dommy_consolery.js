e0b341ae9244d3df2866c630f1571def97c5488a
mayanelson <90730676+mayanelson@users.noreply.github.com>
2023-04-20 09:25:32 -0400

    Update dommy_consolery.js

--- 28_jsdom/dommy_consolery.js
+++ 28_jsdom/dommy_consolery.js
@@ -1,27 +1,7 @@
-/*
-   your PPTASK:
-   
-   Test drive each bit of code in this file,
-    and insert comments galore, indicating anything
-     you discover,
-    	have questions about,
-    		or otherwise deem notable.
-    		
-    		Write with your future self or teammates in mind.
-    		
-    		If you find yourself falling out of flow mode, consult 
-    		other teams
-    		MDN
-
-   A few comments have been pre-filled for you...
-   
-   (delete this block comment once you are done)
-*/
-
-// Team Phantom Tollbooth :: Clyde Sinclair, Fierce Dragon 
-// SoftDev pd0
+// Team TrailBlazers :: Shreya Roy, Jeffrey
+// SoftDev pd2
 // K28 -- Getting more comfortable with the dev console and the DOM
-// 2023-04-05w
+// 2023-04-19
 // --------------------------------------------------
 
 
@@ -30,7 +10,8 @@
 console.log("AYO");
 
 var i = "hello";
-var j = 20;
+const j = 20;
+
 
 
 //assign an anonymous fxn to a var
@@ -51,14 +32,16 @@ var o = { 'name' : 'Thluffy',
         };
 
 
+
 var addItem = function(text) {
   var list = document.getElementById("thelist");
   var newitem = document.createElement("li");
   newitem.innerHTML = text;
   list.appendChild(newitem);
 };
+	//no return statement returns undefined in console
 
-
+	// index n is removed starting from zero (despite browser starting from 1)
 var removeItem = function(n) {
   var listitems = document.getElementsByTagName('li');
   listitems[n].remove();
@@ -83,42 +66,70 @@ var stripe = function() {
     }
   }
 };
+	//cant "reassign" colors if element already has a color class on it
 
 //insert your implementations here for...
-// FIB
-// FAC
-// GCD
+//FAC
+var fac = function(n) {
+	if (n < 2) 
+        return 1;
+      else {
+          return (n * fac(n - 1));
+      }
+}
+
+//FIB
 var fib = function(n){
-    var a = 0;
-    var b = 1;
-    if(n == 0) return a;
-    if(n == 1) return b;
-    for(var i = 0; i < n - 2; i++){
-        var temp = a + b;
-        a = b;
-        b = temp;
-    }
-    return b;
+
+  if (n <= 1){
+	  return n
+  }
+  return fib(n-1)+fib(n-2);
 }
-var fact = function(n){
-    if(n === 0) return 1;
-    return n * fact(n - 1);
+
+// GCD
+var gcd = function(n0,n1){
+	while(!(n0 % n1 == 0)){
+		var f = n0 % n1;
+		n0 = n1;
+		n1 = f;
+	}		
+	return n1;
 }
-var GCD = function(a,b){
-    while(a % b != 0){
-        var  r = a % b;
-        a = b;
-        b = r;
-    }
-    return b
-}
+
 // In addition to the style shown above,
 //  you are encouraged to test drive the "arrow function syntax" as shown below.
 //  Note anything notable.
 const myFxn = (param1, param2) => {
-  // body
-  return retVal;
+  return param1+param2;
 };
-//document.body.innerHTML = "Fifth fibonnaci num is " + fib(5) + "<br>Factorial of 4 is " + fact(4) + 
-document.body.innerHTML = "Fifth fibonnaci num is " + fib(5) + "<br>Factorial of 4 is " + fact(4) + "<br>GCD of 210 and 45 is " + GCD(210,45)
 
+	// arrow syntax does not use function keyword but seems to act the same as a variable assigned to a function
+	// calling the variable myFxn instead of the function myFxn() returns the contents of the function
+	// Q: Const vs var ?
+
+const testAll = () => {
+	return "6th Fib: " + fib(6) + ". 6!: " + fac(6) + ". GCD of 24 and 18: " + gcd(24,18)
+	
+};
+
+
+addItem(testAll())
+
+var button = document.getElementById("fib"); 
+button.addEventListener('click', ()=>{
+	document.getElementById("fibR").innerHTML = fib(15)
+}
+);
+
+var button = document.getElementById("fac"); 
+button.addEventListener('click', ()=>{
+	document.getElementById("facR").innerHTML = fac(6)
+}
+);
+
+var button = document.getElementById("gcd"); 
+button.addEventListener('click', ()=>{
+	document.getElementById("gcdR").innerHTML = gcd(24, 18)
+}
+);
